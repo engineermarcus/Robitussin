a3a681f81576663f23c9be6709739538ce689c19
Marcus <engineermarcus72@gmail.com>
2025-02-25 09:01:57 +0300

    rebase

--- app.js
+++
@@ -1,7 +0,0 @@
-import makeWASocket from 'baileys'
-
-const sock = makeWASocket({
-    // can provide additional config here
-    browser: Browsers.ubuntu('My App'),
-    printQRInTerminal: true
-})
